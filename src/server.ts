f8f0d750bb39adbd6636da69d65e603f242ce241
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-07-29 10:09:18 -0300

    feat: configurando eslint da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello world!'
 })
 
-app.listen({
-  port: 3000
-}).then(() => {
-  console.log('server is running!!!')
-})
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log('server is running!!!')
+  })
