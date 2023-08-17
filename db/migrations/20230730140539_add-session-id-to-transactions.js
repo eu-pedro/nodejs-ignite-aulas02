d0c515b53f8596d5d02d90074d18480632e82a2e
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-17 20:24:34 -0300

    feat: preparando o app para deploy

---
+++ db/migrations/20230730140539_add-session-id-to-transactions.js
@@ -0,0 +1,16 @@
+"use strict";
+/* eslint-disable prettier/prettier */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.down = exports.up = void 0;
+async function up(knex) {
+    await knex.schema.alterTable('transactions', (table) => {
+        table.uuid('session_id').after('id').index();
+    });
+}
+exports.up = up;
+async function down(knex) {
+    await knex.schema.alterTable('transactions', (table) => {
+        table.dropColumn('session_id');
+    });
+}
+exports.down = down;
